a0ee7b81d9d226106969e4cd77211f910c569509
KristianTV <75727323+KristianTV@users.noreply.github.com>
2025-07-28 19:40:00 +0300

    Create Notification.cs

---
+++ OrderFlow.Data.Models/Notification.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace OrderFlow.Data.Models
+{
+    public class Notification
+    {
+        public Guid Id { get; set; }
+
+        public string Title { get; set; } = string.Empty;
+        public string Message { get; set; } = string.Empty;
+
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+
+        public bool IsRead { get; set; } = false;
+
+        public Guid ReceiverId { get; set; }
+        public IdentityUser Receiver { get; set; } = null!;
+
+        public Guid? SenderId { get; set; }
+        public IdentityUser? Sender { get; set; }
+
+        public Guid? OrderId { get; set; }
+        public Order? Order { get; set; }
+    }
+}
