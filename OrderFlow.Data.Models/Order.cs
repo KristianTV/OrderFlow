24565454baf6f0683e09987ff4779bebd1779c51
KristianTV <75727323+KristianTV@users.noreply.github.com>
2025-07-28 19:24:11 +0300

    Add Order model and EF Core dependencies

---
+++ OrderFlow.Data.Models/Order.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace OrderFlow.Data.Models
+{
+    public class Order
+    {
+        public Guid OrderID { get; set; } = Guid.NewGuid();
+
+        public Guid UserID { get; set; }
+        public IdentityUser User { get; set; } = null!;
+
+        public DateTime OrderDate { get; set; } = DateTime.UtcNow;
+        public DateTime DeliveryDate { get; set; } = DateTime.UtcNow;
+
+        public string DeliveryAddress { get; set; } = string.Empty;
+        public string PickupAddress { get; set; } = string.Empty;
+
+        public string? DeliveryInstructions { get; set; }
+
+        public string Status { get; set; } = "Pending";
+        
+        public bool isCanceled { get; set; } = false;
+    }
+}
