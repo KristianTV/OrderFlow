d05e16aabe280b5bc30fe637183ef6bf888b597b
KristianTV <75727323+KristianTV@users.noreply.github.com>
2025-07-28 19:45:09 +0300

    Add Order entity configuration and update DbContext

---
+++ OrderFlow.Data/Configuration/OrderConfiguration.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using OrderFlow.Data.Models;
+
+namespace OrderFlow.Data.Configuration
+{
+    internal class OrderConfiguration : IEntityTypeConfiguration<Order>
+    {
+        public void Configure(EntityTypeBuilder<Order> builder)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- OrderFlow.Data/OrderFlowDbContext.cs
+++ OrderFlow.Data/OrderFlowDbContext.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using OrderFlow.Data.Configuration;
+using OrderFlow.Data.Models;
 
 namespace OrderFlow.Data
 {
@@ -8,6 +10,18 @@ namespace OrderFlow.Data
         public OrderFlowDbContext(DbContextOptions<OrderFlowDbContext> options)
           : base(options)
         {
+        }
+
+        public virtual DbSet<Notification> Notifications { get; set; } = null!;
+        public virtual DbSet<Order> Orders { get; set; } = null!;
+        public virtual DbSet<TruckOrder> TrucksOrders { get; set; } = null!;
+        public virtual DbSet<Truck> Trucks { get; set; } = null!;
+        public virtual DbSet<Payment> Payments { get; set; } = null!;
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            builder.ApplyConfigurationsFromAssembly(typeof(OrderConfiguration).Assembly);
         }
     }
 }
