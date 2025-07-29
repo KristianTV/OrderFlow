9065267a270c105fc1d9706098bb7252c465b8d1
KristianTV <75727323+KristianTV@users.noreply.github.com>
2025-07-29 10:24:35 +0300

    Add BaseController and update HomeController inheritance

---
+++ OrderFlow/Controllers/BaseController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+
+namespace OrderFlow.Controllers
+{
+    [Authorize]
+    public class BaseController : Controller
+    {
+        protected bool IsUserAuthenticated()
+        {
+            return User.Identity?.IsAuthenticated ?? false;
+        }
+
+        protected string? GetUserId()
+        {
+            bool isAuthenticated = this.IsUserAuthenticated();
+            if (!isAuthenticated)
+            {
+                return null;
+            }
+
+            return User.FindFirstValue(ClaimTypes.NameIdentifier);
+        }
+    }
+}
