5e5a312d38a9fb860e47f31b897f736b91a52b5f
KristianTV <75727323+KristianTV@users.noreply.github.com>
2025-07-29 09:39:34 +0300

    Add base repository and IRepository interface

---
+++ OrderFlow.Data/Repository/BaseRepository.cs
@@ -0,0 +1,43 @@
+﻿using OrderFlow.Data.Repository.Contracts;
+using Microsoft.EntityFrameworkCore;
+
+namespace OrderFlow.Data.Repository
+{
+    public abstract class BaseRepository(DbContext _context) : IRepository
+    {
+        protected DbSet<T> DbSet<T>() where T : class
+        {
+            return _context.Set<T>();
+        }
+
+        public async Task AddAsync<T>(T entity) where T : class
+        {
+            await DbSet<T>().AddAsync(entity);
+        }
+
+        public void Delete<T>(T entity) where T : class
+        {
+            DbSet<T>().Remove(entity);
+        }
+
+        public async Task<bool> ExistsAsync<T>(int id) where T : class
+        {
+           return await _context.Set<T>().FindAsync(id) == null;
+        }
+
+        public async Task<int> SaveChangesAsync()
+        {
+          return await _context.SaveChangesAsync();
+        }
+
+        public async Task AddRangeAsync<T>(IEnumerable<T> entities) where T : class
+        {
+            await DbSet<T>().AddRangeAsync(entities);
+        }
+
+        public IQueryable<T> All<T>() where T : class
+        {
+           return DbSet<T>().AsQueryable();
+        }
+    }
+}
---
+++ OrderFlow.Data/Repository/Contracts/IRepository.cs
@@ -0,0 +1,12 @@
+﻿namespace OrderFlow.Data.Repository.Contracts
+{
+    public interface IRepository
+    {
+        Task AddAsync<T>(T entity) where T : class;
+        Task AddRangeAsync<T>(IEnumerable<T> entities) where T : class;
+        IQueryable<T> All<T>() where T : class;
+        void Delete<T>(T entity) where T : class;
+        Task<bool> ExistsAsync<T>(int id) where T : class;
+        Task<int> SaveChangesAsync();
+    }
+}
