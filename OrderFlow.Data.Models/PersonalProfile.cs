8f3c04419fa4f7b83ccdc03e6fa23d6a95d50775
KristianTV <75727323+KristianTV@users.noreply.github.com>
2026-02-04 09:31:38 +0200

    Add two new profiles types

---
+++ OrderFlow.Data.Models/PersonalProfile.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace OrderFlow.Data.Models
+{
+    public class PersonalProfile
+    {
+        public Guid UserId { get; set; }
+        public ApplicationUser? User { get; set; } = null!;
+
+        [ProtectedPersonalData]
+        public string FirstName { get; set; } = null!;
+
+        [ProtectedPersonalData]
+        public string LastName { get; set; } = null!;
+
+        [ProtectedPersonalData]
+        public string PersonalNumber { get; set; } = null!;
+
+        [ProtectedPersonalData]
+        public string Adress { get; set; } = null!;
+
+        [ProtectedPersonalData]
+        public DateTime? DateOfBirth { get; set; }
+    }
+}
---
+++ OrderFlow.Data/Configuration/PersonalProfileConfiguration.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using OrderFlow.Data.Models;
+using OrderFlow.GCommon;
+
+namespace OrderFlow.Data.Configuration
+{
+    public class PersonalProfileConfiguration : IEntityTypeConfiguration<PersonalProfile>
+    {
+        public void Configure(EntityTypeBuilder<PersonalProfile> builder)
+        {
+            builder.HasKey(pp => pp.UserId);
+
+            builder.Property(pp => pp.FirstName)
+                   .IsRequired()
+                   .HasMaxLength(ValidationConstants.PersonalProfile.FirstNameMaxLength);
+
+            builder.Property(pp => pp.LastName)
+                   .IsRequired()
+                   .HasMaxLength(ValidationConstants.PersonalProfile.LastNameMaxLength);
+
+            builder.Property(pp => pp.PersonalNumber)
+                   .IsRequired()
+                   .HasMaxLength(ValidationConstants.PersonalProfile.PersonalNumberMaxLength);
+
+            builder.Property(pp => pp.Adress)
+                   .IsRequired()
+                   .HasMaxLength(ValidationConstants.PersonalProfile.AdressMaxLength);
+
+            builder.Property(builder => builder.DateOfBirth)
+                   .IsRequired(false);
+
+            builder.HasOne(pp => pp.User)
+                   .WithOne(u => u.PersonalProfile)
+                   .HasForeignKey<PersonalProfile>(pp => pp.UserId)
+                   .OnDelete(DeleteBehavior.Cascade);
+        }
+    }
+}
