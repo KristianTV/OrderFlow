8f06177dbc676b6c6d276c1d258d7d51fe5b6233
KristianTV <75727323+KristianTV@users.noreply.github.com>
2026-02-11 09:51:36 +0200

    Add Notification command

---
+++ OrderFlow.Services.Core/Commands/NotificationCommand.cs
@@ -0,0 +1,13 @@
+﻿namespace OrderFlow.Services.Core.Commands
+{
+    public class NotificationCommand
+    {
+        public string Title { get; set; } = null!;
+        public string Message { get; set; } = null!;
+        public Guid ReceiverID { get; set; }
+        public Guid? OrderID { get; set; }
+        public Guid? CourseID { get; set; }
+        public Guid? TruckID { get; set; }
+        public bool CanRespond { get; set; } = false;
+    }
+}
