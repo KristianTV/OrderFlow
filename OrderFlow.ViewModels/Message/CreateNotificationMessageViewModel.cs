4c925ec0740903768bd52821443ba193a4d041fe
KristianTV <75727323+KristianTV@users.noreply.github.com>
2026-04-17 13:40:55 +0300

    Add message view models

---
+++ OrderFlow.ViewModels/Message/CreateNotificationMessageViewModel.cs
@@ -0,0 +1,21 @@
+﻿using OrderFlow.GCommon;
+using System.ComponentModel.DataAnnotations;
+
+namespace OrderFlow.ViewModels.Message
+{
+    public class CreateNotificationMessageViewModel
+    {
+        [StringLength(ValidationConstants.Message.ContentMaxLength,
+                     MinimumLength = ValidationConstants.Message.ContentMinLength,
+                     ErrorMessage = ErrorMessages.StringLengthRange)]
+        [Required(ErrorMessage = ErrorMessages.PropertyIsRequired)]
+        [Display(Name = "Message Content")]
+        public string Content { get; set; } = string.Empty;
+
+        public Guid? SenderID { get; set; }
+
+        public Guid? ReceiverID { get; set; }
+
+        public Guid? NotificationID { get; set; }
+    }
+}
