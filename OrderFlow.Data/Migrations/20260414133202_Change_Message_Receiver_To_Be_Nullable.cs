af3acd223744a0f2a3a10105753702e28b64b707
KristianTV <75727323+KristianTV@users.noreply.github.com>
2026-04-17 09:10:24 +0300

    Change message receiver to be optional

---
+++ OrderFlow.Data/Migrations/20260414133202_Change_Message_Receiver_To_Be_Nullable.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace OrderFlow.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class Change_Message_Receiver_To_Be_Nullable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<Guid>(
+                name: "ReceiverID",
+                table: "Messages",
+                type: "uniqueidentifier",
+                nullable: true,
+                oldClrType: typeof(Guid),
+                oldType: "uniqueidentifier");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<Guid>(
+                name: "ReceiverID",
+                table: "Messages",
+                type: "uniqueidentifier",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"),
+                oldClrType: typeof(Guid),
+                oldType: "uniqueidentifier",
+                oldNullable: true);
+        }
+    }
+}
