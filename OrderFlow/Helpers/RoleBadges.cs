3e1ee2b367839337692d4b0884c0d3d817483fd8
KristianTV <75727323+KristianTV@users.noreply.github.com>
2026-05-18 10:13:28 +0300

    Create RoleBadges.cs

---
+++ OrderFlow/Helpers/RoleBadges.cs
@@ -0,0 +1,29 @@
+﻿namespace OrderFlow.Helpers
+{
+    public static class RoleBadges
+    {
+        public static string GetRoleBadgeClass(string role)
+        {
+            return role switch
+            {
+                "Admin" => "role-badge-admin",
+                "Driver" => "role-badge-driver",
+                "Speditor" => "role-badge-speditor",
+                "User" => "role-badge-user",
+                _ => "role-badge-default"
+            };
+        }
+
+        public static string GetRoleIconClass(string role)
+        {
+            return role switch
+            {
+                "Admin" => "fas fa-shield-alt",
+                "Driver" => "fas fa-truck",
+                "Speditor" => "fas fa-boxes",
+                "User" => "fas fa-user",
+                _ => "fas fa-tag"
+            };
+        }
+    }
+}
