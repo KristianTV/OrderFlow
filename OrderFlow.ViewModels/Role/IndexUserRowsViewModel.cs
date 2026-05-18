f05046eff8acfb003a3e18d7a4b3d9986d92dd38
KristianTV <75727323+KristianTV@users.noreply.github.com>
2026-05-18 09:15:39 +0300

    Update IndexUserRowsViewModel

--- OrderFlow.ViewModels/Role/IndexUserRowsViewModel.cs
+++ OrderFlow.ViewModels/Role/IndexUserRowsViewModel.cs
@@ -4,6 +4,9 @@
     {
         public Guid UserId { get; set; }
         public string UserName { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public string DisplayName { get; set; } = string.Empty;
+        public string AccountType { get; set; } = string.Empty;
         public IEnumerable<string> Roles { get; set; } = new List<string>();
     }
 }
