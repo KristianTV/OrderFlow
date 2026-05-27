96255aeb4783ecd73341387ea287e3d2fea968c6
KristianTV <75727323+KristianTV@users.noreply.github.com>
2026-05-27 09:40:23 +0300

    Update TruckSpendingQueryModel.cs

--- OrderFlow.ViewModels/TruckSpending/TruckSpendingQueryModel.cs
+++ OrderFlow.ViewModels/TruckSpending/TruckSpendingQueryModel.cs
@@ -2,12 +2,16 @@ namespace OrderFlow.ViewModels.TruckSpending
 {
     public class TruckSpendingQueryModel
     {
-        public Guid? TruckID { get; set; }
+        public Guid? TruckID { get; set; } = null;
 
-        public Guid? TruckCourseID { get; set; }
+        public Guid? TruckCourseID { get; set; } = null;
 
-        public string? Search { get; set; }
+        public string? Search { get; set; } = null;
 
-        public string? SortOrder { get; set; }
+        public string? SortOrder { get; set; } = null;
+
+        public int Page { get; set; } = 1;
+
+        public int PageSize { get; set; } = 12;
     }
 }
